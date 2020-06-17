acbdfa378b62a8f96f54a25f5883698ab4045c0e
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-06-17 13:10:22 +0100

    IT json sample file added

--- src/main/java/com/company/Json/AssetRegister.java
+++ src/main/java/com/company/Json/AssetRegister.java
@@ -1,11 +1,42 @@
 package com.company.Json;
 
+import com.google.gson.Gson;
+
 public class AssetRegister {
-    private String id;
+        private long id;
+        private String name;
 
-    public AssetRegister(String id){
-        this.id = id;
+        public long getId() {
+            return id;
+        }
+        public void setId(long id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
 
+        @Override
+        public String toString() {
+            return "User [id=" + id + ", name=" + name + "]";
+        }
+
+public static void jasoncall () {
+    String userJson = "[{'name': 'Alex','id': 1}, "
+            + "{'name': 'Brian','id':2}, "
+            + "{'name': 'Charles','id': 3}]";
+
+    Gson gson = new Gson();
+
+    AssetRegister[] assetRegisterArray = gson.fromJson(userJson, AssetRegister[].class);
+
+    for (AssetRegister assetRegister : assetRegisterArray) {
+        System.out.println(assetRegister);
     }
 
+
+}
 }
