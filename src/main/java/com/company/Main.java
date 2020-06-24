3eeb693807bd64db10acf751fdda41fcc9fe361a
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-06-24 22:16:15 +0100

    Main class uploaded

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,22 +1,47 @@
 package com.company;
 
-import com.company.Json.AssetRegister;
-import com.google.gson.Gson;
+import com.company.Json.*;
+import com.google.gson.internal.bind.util.ISO8601Utils;
 
-import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Loading items....");
-        deserializeAssetRegister();
+
+       JsonParser employee = new JsonParser(){
+
+           Scanner scanner = new Scanner(System.in);
+
+           public Scanner getScanner() {
+               scanner.nextLine();
+               return scanner;
+           }
+       };
+
+        System.out.println("\nWelcome to the Asset Register.  \n"
+                + "Please search on the following option\n"
+                + "1 - IT Team\n"
+                + "2 - Admin Team");
+        String teamSelection = scanner.nextLine();
+        if (teamSelection.equals("1"));
+        {
+            System.out.println("\nPlease Enter the TNS Asset No. \n");
+            String selectNumber = scanner.nextLine();
+            employee.itItem(selectNumber);
+            employee.itElements();
+            employee.itMapping();
+        }
+
+
+
+
+
     }
 
-    private static void deserializeAssetRegister(){
-        Gson gson = new Gson();
-        gson.fromJson(, AssetRegister.class);
+}
 
-    }
-    }
 
