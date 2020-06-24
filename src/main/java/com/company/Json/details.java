160679f33d224981a40064c8143a3d76b2f4ce1a
Martin Burns <burns3491@gmail.com>
2020-06-24 15:59:36 +0100

    MB

--- src/main/java/com/company/Json/details.java
+++ src/main/java/com/company/Json/details.java
@@ -1,4 +1,17 @@
 package com.company.Json;
 
 public class details {
+        private String security;
+        private String retention;
+        private String location;
+
+        public Assets(String security, String retention, String location) {
+            this.security = security;
+            this.retention = retention;
+            this.location = location;
+
+        }
+
+
+
 }
