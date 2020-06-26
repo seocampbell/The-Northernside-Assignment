b6ed9b22313dc4e1f6ad3aae5ff6b24a9cc1c50a
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-06-26 21:30:19 +0100

    Main class uploaded

--- src/main/java/com/company/Json/Department.java
+++ src/main/java/com/company/Json/Department.java
@@ -1,9 +1,43 @@
 package com.company.Json;
 
 public class Department {
-        private String name;
 
-        public Department(String name){
-            this.name = name;
-        }
+    private String departmentSelector;
+    public String jsonFileName;
+
+    public void setDepartmentSelector(String departmentSelector) {
+        String ValidateResource = departmentSelector;
+        if (ValidateResource.equals("staff") ||
+                ValidateResource.equals("assets")) {
+            this.departmentSelector = departmentSelector;
+        } else System.out.println("Department does not exist, please check.");
     }
+
+
+    public String getDepartmentSelector() {
+        return this.departmentSelector;
+    }
+
+    public String getJsonFileName() {
+        return jsonFileName;
+    }
+
+    public void setJsonFileName(String jsonFileName) {
+        String ValidateLocation = jsonFileName;
+        if (ValidateLocation.equals("src/IT_Assets.json") ||
+                ValidateLocation.equals("src/Admin_Assets.json")) {
+            this.jsonFileName = jsonFileName;
+        } else System.out.println("Error! The file location is incorrect!");
+    }
+
+    public Department(String departmentSelector, String jsonFileName) {
+        String validateSelection = departmentSelector;
+        if (validateSelection.equals("staff") ||
+                validateSelection.equals("assets")) {
+            this.jsonFileName = jsonFileName;
+        } else System.out.println("Error! The array selector has not been set!");
+        this.departmentSelector = departmentSelector;
+    }
+
+
+}
