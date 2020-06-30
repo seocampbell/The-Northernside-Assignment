3968a31c2940853849ae47afc509cbb24057017f
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-06-30 20:21:35 +0100

    Main class uploaded

--- src/main/java/com/company/Json/Department.java
+++ src/main/java/com/company/Json/Department.java
@@ -5,14 +5,14 @@ public class Department {
     private String departmentSelector;
     public String jsonFileName;
 
-
+    //From the main selector, the setter will validate the user choice - sc
     public void setDepartmentSelector(int departmentSelector) {
         int ValidateResource = departmentSelector;
         if (ValidateResource==1) {
             this.departmentSelector = "staff";
         } else if(ValidateResource==2)
-        { this.departmentSelector = "staff";
-        } else System.out.println("Error, the department has not been expected, please check.");
+        { this.departmentSelector = "assets";
+        } else System.out.println("Error, please check your selection and try again.");
     }
 
     public String getDepartmentSelector()
@@ -24,6 +24,7 @@ public class Department {
     {
         return this.jsonFileName;
     }
+    //From the main selector, the setter will validate the user choice and set the location of the file - sc
     public void setJsonFileName(int jsonSelector) {
         int validateLocation = jsonSelector;
         if (validateLocation==1) {
