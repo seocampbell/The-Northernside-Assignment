4920fc664b0d601d445ecc496d04ac3cfdb0ebc5
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-06-30 20:21:50 +0100

    Main class uploaded

--- src/main/java/com/company/Json/Users.java
+++ src/main/java/com/company/Json/Users.java
@@ -1,9 +1,10 @@
 package com.company.Json;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class Users {
 
-    public String AssetNumber;
-
+    private String assetNumber;
     //WelcomeMessage screen
     public void welcomeMessage(){
         System.out.println("\nWelcome to the Asset Register. \n " +
@@ -13,34 +14,40 @@ public class Users {
     public void departmentOptions(){
         System.out.println("Please select one of the following options\n"
                 + "1 - IT Team\n"
-                + "2 - Admin Team");
+                + "2 - Admin Team\n"
+                + "3 - Instructions");
     }
     //Read me option for instructions
     public void readMe(){
         System.out.println("Welcome to the Asset Management System \n" +
-                "Read me \n" +
+                "The Asset Management Registry has been set up to search from  \n" +
                     "Read me");
     }
-    //Asset search option message
+    //Message for the asset search option message
     public void searchCriteria(){
         System.out.println("Please select one of the " +
                 "following options to view the User or Asset details. \n"
                 + "1 - Users Details\n"
                 + "2 - Systems Details");
     }
+    //Message to enter the TNS number
+    public void assetSearch(){
+        System.out.println("Please enter a valid TNS Asset Number, " +
+                "please include the TNS");
+    }
 
     public String getAssetNumber() {
-        return this.AssetNumber;
-    }
-    //Constructor set up for get the  Asset Number from the search - SC
-    public Users(String assetNumber) {
-        this.AssetNumber = assetNumber;
+        return assetNumber;
     }
 
     public void setAssetNumber(String assetNumber) {
-        this.AssetNumber = assetNumber;
-
-
+        String validateAsset = assetNumber;
+        if (validateAsset.contains("TNS"))
+        { this.assetNumber = assetNumber;
+        } else {
+            System.out.println("The TNS number was missing, or the field was left blank.");
+            assetNumber = "";
+        }
     }
 }
 
