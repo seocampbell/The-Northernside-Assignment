823326ef8462864eeb85f8c5c682273ec143dde6
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-07-03 12:32:11 +0100

    Main class uploaded

--- src/main/java/com/company/Json/Department.java
+++ src/main/java/com/company/Json/Department.java
@@ -5,37 +5,36 @@ public class Department {
     private String departmentSelector;
     public String jsonFileName;
 
-    //From the main selector, the setter will validate the user choice - sc
+    //setters and getters for the main selector, the setter will validate the user choice with validation checks - sc
     public void setDepartmentSelector(int departmentSelector) {
-        int ValidateResource = departmentSelector;
-        if (ValidateResource==1) {
+        int validateResource = departmentSelector;
+        if (validateResource == 1) {
             this.departmentSelector = "staff";
-        } else if(ValidateResource==2)
-        { this.departmentSelector = "assets";
+        } else if (validateResource == 2) {
+            this.departmentSelector = "assets";
         } else System.out.println("Error, please check your selection and try again.");
     }
 
-    public String getDepartmentSelector()
-    {
+    public String getDepartmentSelector() {
         return this.departmentSelector;
     }
 
-    public String getJsonFileName()
-    {
+    public String getJsonFileName() {
         return this.jsonFileName;
     }
+
     //From the main selector, the setter will validate the user choice and set the location of the file - sc
     public void setJsonFileName(int jsonSelector) {
         int validateLocation = jsonSelector;
-        if (validateLocation==1) {
-          this.jsonFileName = "src/IT_Assets.json";
-        } else if(validateLocation==2)
-        { this.jsonFileName = "src/Admin_Assets.json";
-        }else System.out.println("Error! The file location is incorrect!");
+        if (validateLocation == 1) {
+            this.jsonFileName = "src/IT_Assets.json";
+        } else if (validateLocation == 2) {
+            this.jsonFileName = "src/Admin_Assets.json";
+        } else System.out.println("Error! The file location is incorrect!");
 
     }
-    public Department(String departmentSelector, String jsonFileName)
-    {
+
+    public Department(String departmentSelector, String jsonFileName) {
         this.departmentSelector = departmentSelector;
     }
 
