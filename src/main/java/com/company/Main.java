0e4a82350427db381f29452ef7fb9983c783bff7
Stephen Campbell <se.campbell@hotmail.co.uk>
2020-07-03 12:33:00 +0100

    Main class uploaded

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,48 +2,46 @@ package com.company;
 
 import com.company.Json.*;
 import com.company.Operations.Usage;
-
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        //comments to come - SC
-
 
         int teamSelection;
         String userInput;
         String selectNumber;
 
-        //Constructor created for the department and json file
+        //Constructor created for the department and json file -sc
         Department selector = new Department(null, null);
         Scanner scanner = new Scanner(System.in);
+        //Constructor set for the data serialisation - sc
         JsonParser assetDetails = new JsonParser();
-        //Constructor set for the user messages
+        //Constructor set for the user messages - sc
         Users welcome = new Users();
-        //Constructor set for the username and password
-        Usage userAuthenticator = new Usage(null,null);
+        //Constructor set for the username and password - sc
+        Usage userAuthenticator = new Usage(null, null);
 
         //Welcome screen to present the user options - sc
         welcome.welcomeMessage();
-        //loop to repeat the username entry until true
+        //loop to repeat the username entry until true - sc
         do {
             //details set for prototype only - sc
             System.out.println("Username: (Username is Test)");
             userInput = scanner.nextLine();
             userAuthenticator.setUserName(userInput);
-        }while (userInput.equals(true));
+        } while (userInput.equals(true));
 
-        //loop to repeat the username entry until true
+        //loop to repeat the username entry until true - sc
         do {
             //details set for prototype only
             System.out.println("Password: (Password is Password)");
             userInput = scanner.nextLine();
             userAuthenticator.setUserPassword(userInput);
-        }while (userInput.equals(true));
+        } while (userInput.equals(true));
 
-        //Creating a loop to make sure only the available options are selected and correct file and department to be selected - sc
+        //Creating a loop to make sure only the available - sc
+        // options are selected and correct file and department to be selected - sc
         do {
             welcome.departmentOptions();
             teamSelection = scanner.nextInt();
@@ -54,7 +52,8 @@ public class Main {
             } else System.out.println("Unfortunately, that is not one of the options\n");
         } while (teamSelection > 2);
 
-        //Creating a loop to make sure only the available options are selected and correct User or System details and selected - sc
+        //Creating a loop to make sure only the available
+        // options are selected and correct User or System details and selected - sc
         do {
             welcome.searchCriteria();
             teamSelection = scanner.nextInt();
@@ -66,7 +65,7 @@ public class Main {
         } while (teamSelection > 2);
 
         //Call the first method in the JsonParser to set the locations - sc
-        assetDetails.assetList(selector.getDepartmentSelector(),selector.getJsonFileName());
+        assetDetails.assetList(selector.getDepartmentSelector(), selector.getJsonFileName());
 
         //User to add a TNS number
         welcome.assetSearch();
@@ -76,12 +75,10 @@ public class Main {
         selectNumber = scanner.nextLine();
         welcome.setAssetNumber(selectNumber);
         selectNumber = welcome.getAssetNumber();
-        while(selectNumber==null||selectNumber.isEmpty()){
+        while (selectNumber == null || selectNumber.isEmpty()) {
             System.out.println("Please re-enter a valid TNS Asset Number");
             selectNumber = scanner.nextLine();
         }
-        //System.out.println(selectNumber);
-
         //Call the 2nd method in JsonParser - sc
         assetDetails.itItem(selectNumber);
 
